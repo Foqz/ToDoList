ffed89dd3315e2924f55d06079620793eb646fb9
Kamil Fiks <fikskamil@gmail.com>
2019-02-19 12:09:00 +0100

    delete and add list element

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,22 @@
 // Check Off specific Todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
+})
+
+//Click on x to delete to do
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+        var toDoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X </span>"+ toDoText + "</li>")
+        
+    }
 })
